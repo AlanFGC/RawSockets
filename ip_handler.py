e3772cac67f772d9f9774f5708519e8ccbd7ca9f
ehhaines <haines.e@northeastern.edu>
2022-10-26 19:45:09 +0000

    Hardcode all headers except checksum, source IP, and padding

--- ip_handler.py
+++ ip_handler.py
@@ -1,7 +1,8 @@
 import socket
 import sys
+import random
 
-def decimal_to_binary(address: str):
+def address_to_binary(address: str):
     '''
     Takes an IP address in decimal notation as a string
     and returns the same IP address in binary.
@@ -31,8 +32,28 @@ def decimal_to_binary(address: str):
     return out
     
 
+def decimal_to_binary(value, num_digits):
 
-def main():
+    bits = []
+    while value:
+        bits.append(value % 2)
+        value = value // 2
+
+    missing = num_digits - len(bits)
+    while missing:
+        bits.append(0)
+        missing -= 1
+    
+    bits.reverse()
+    
+    out = ""
+    for i in bits:
+        out += str(i)
+    
+    return out
+
+
+def make_ip_header(data):
 
 # 0                   1                   2                   3
 # 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
@@ -59,10 +80,28 @@ def main():
     version = "0010"
 
     dest = socket.gethostbyname(sys.argv[1])
-    dest = decimal_to_binary(dest)
+    dest = address_to_binary(dest)
 
-    ihl = None  # computer after all variables initialized
-    
+    type_of_service = "00100000"    # https://www.omnisecu.com/tcpip/ipv4-protocol-and-ipv4-header.php#:~:text=%22Type%20of%20Service%20(ToS),delay%2C%20throughput%2C%20and%20reliability.
+
+    identifier = random.randint(0, 2**16)
+    identifer = decimal_to_binary(identifer, 16)
+
+    flag = "000"
+    fragment_offset = "0000000000000"
+
+    ttl = "00010000"    # 32 hops should be enough, right?
+
+    protocol = "00000110"   # 6 = TCP
+
+    ihl = None  # compute after all variables initialized
+
+    total_length = ihl + len(data) / 8
+
+
+
+    check_sum = "00000000"
+
 
 if __name__ == "__main__":
-    main()
+    make_ip_header("11111111")
