713f866d266e55edc6c4d97a1e512eb32b7bfd83
ehhaines <haines.e@northeastern.edu>
2022-10-31 05:54:14 +0000

    Make sock_send an IPPROTO_RAW socket

--- rawhttpget.py
+++ rawhttpget.py
@@ -5,14 +5,10 @@ from subprocess import check_output
 def main(packet):
     
     #s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.IPPROTO_RAW)
-    sock_send = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.SOCK_STREAM)
+    sock_send = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
     sock_rec = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    print(s)
-
-    sock_send.sendall(packet)
+    #sock_send.sendall(packet)
 
     # https://stackoverflow.com/questions/24196932/how-can-i-get-the-ip-address-from-a-nic-network-interface-controller-in-python
     ips = check_output(['hostname', '--all-ip-addresses']).decode()
@@ -23,4 +19,4 @@ def main(packet):
     # sock_send.sendall(message)
 
 if __name__ == "__main__":
-    main()
+    main(None)
