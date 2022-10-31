d561eac8b2a5765ffdf00913c0887a63249547b9
ehhaines <haines.e@northeastern.edu>
2022-10-31 16:42:39 +0000

    Fix IP checksum

--- checksum.py
+++ checksum.py
@@ -1,28 +1,5 @@
- 
-example = "0100010100000000000000000011110000011100010001100100000000000000010000000000011000000000000000001010110000010000000010100110001110101100000100000000101000001100"
-
-def add_binary(bin1, bin2):
-
-    dec1 = int(bin1, 2)
-    dec2 = int(bin2, 2)
-    total = dec1 + dec2
-    total = bin(total)
-    if len(total) < len(bin1):
-        prepend = "0" * (len(bin1) - len(total))
-        total = "0b" + prepend + total[2:]
-    if len(total) > len(bin1):
-        keep = "0b" + total[3:]
-        keep = int(keep, 2) + 1
-        total = bin(keep)[2:]
-        prepend = "0" * (16 - len(total))
-        total = "0b" + prepend + total
-
-    return total
-
 def calculate_checksum(header):
-    '''
-    Checksum only applies to the first 5 lines of the header.
-    '''
+    
     groups = []
     start = 0
     end = 16
@@ -31,15 +8,33 @@ def calculate_checksum(header):
         start += 16
         end += 16
     
-    checksum = groups[0]
-    for i in range(1, len(groups)):
-        checksum = add_binary(checksum, groups[i])
+    total = 0
+
+    for i in range(len(groups)):
+        total += int(groups[i], 2)
     
-    checksum = checksum.replace("0b", "")
-    print(checksum)
-    print(len(checksum))
+    total = bin(total)[2:]
+
+    diff = len(total) - 16
+    
+    if diff > 0:
+        total = bin(int(total[:diff], 2) + int(total[diff:], 2))[2:]
+    
+    diff = 16 - len(total)
+    total = "0" * diff + total
+
+    out = ""
+
+    for char in total:
+        if char == "1":
+            out += "0"
+        else:
+            out += "1"
+
+    return out
         
 
 
 if __name__ == "__main__":
-    calculate_checksum(example)
+    example2 = "0100010100000000000000000011010000101111011100000100000000000000010000000000011000000000000000001100000010101000010000000000011100100010011010111101110101010010"
+    calculate_checksum(example2)
