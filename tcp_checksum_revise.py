3bb4fa9531638482ab97948662f00bd5104f5140
ehhaines <haines.e@northeastern.edu>
2022-11-04 23:26:00 +0000

    Run minor (now deleted) test

--- tcp_checksum_revise.py
+++ tcp_checksum_revise.py
@@ -1,4 +1,5 @@
 import array
+import struct
 
 def create_tcp_checksum(init_packet):
 
@@ -10,7 +11,7 @@ def create_tcp_checksum(init_packet):
     num_bits = total.bit_length()
 
     difference = 0
-    if len(num_bits) > 16:
+    if num_bits > 16:
         difference = len(total) - 16
     
     overflow = total >> 16
@@ -19,3 +20,41 @@ def create_tcp_checksum(init_packet):
     total = overflow + main
 
     return ~total
+
+if __name__ == "__main__":
+
+    class TCPPacket:
+        def __init__(self,
+                    src_host,
+                    src_port,
+                    dst_host,
+                    dst_port,
+                    flags=0):
+            self.src_host = src_host
+            self.src_port = src_port
+            self.dst_host = dst_host
+            self.dst_port = dst_port
+            self.flags = flags
+        
+        def build(self):
+            packet = struct.pack(
+                '!HHIIBBHHH',
+                self.src_port,  # Source Port
+                self.dst_port,  # Destination Port
+                0,              # Sequence Number
+                0,              # Acknoledgement Number
+                5 << 4,         # Data Offset
+                self.flags,     # Flags
+                8192,           # Window
+                0,              # Checksum (initial value)
+                0               # Urgent pointer
+            )
+            return packet
+
+    pack = TCPPacket(19945, 5, 338586, 997, 3)
+    pack = pack.build()
+
+    bleh = create_tcp_checksum(pack)
+    print(bleh)
+
+    
