899010b20a9dc771f7823be62000213649f83c93
Alan <77854728+AlanFGC@users.noreply.github.com>
2022-11-07 19:15:02 -0500

    Added a new connection class

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import socket
 import ip_handler
 import random
 import httpGet
+import threading
+import queue
 
 """
 Evan Hanes
@@ -20,23 +22,56 @@ def main(domain: str):
     domain = "www.david.choffnes.com"
     dest_ip = socket.gethostbyname(domain)
     dest_port = 80
-    sendS, recS,  handshake(dest_ip, dest_port, local_ip, domain)
+    sendSoc, recSoc = handshake(dest_ip, dest_port, local_ip, domain, "/")
     
     
-    sock_send.close()
-    sock_rec.close()
+    sendSoc.close()
+    recSoc.close()
+    
+    
+def download(conn: Connection):
+    conn = Connection()
+    download = {} #sequence Number: RAW DATA
+    workList = queue.Queue()
+    
+    # Send GET http request
+    packet = httpGet.craftRequest(conn.domain, conn.subdomain)
+    packet = ip_handler.make_tcp_header_2(packet , conn.rec_port, conn.dest_port, conn.seq_numb + 1, conn.ack_numb + 1, 1, True, False, False, conn.local_ip, conn.dest_ip)
+    packet = ip_handler.make_ip_header(packet, conn.local_ip, conn.dest_ip)
+    conn.send_sock.sendto(packet, (conn.dest_ip, conn.dest_port))
+    
+    
+    window = 1
+    # create a thread that listens to all
+    while len(workList > 0):
+        pass
+    
+    
+    # wait until both processes are done
+    workList.join()
+    
+    
+    return download
     
     
     
+
+
+def packetReciever(sock: socket, workList: queue):
     
+    fin = False;
+    while not fin:
+        packet = sock.recv(1500)
+        workList.enqueue
+    pass
     
     
         
     
-def handshake(dest_ip, dest_port, local_ip, domain):
+def handshake(dest_ip, dest_port, local_ip, domain, subdomain):
     # HANDSHAKE
     
-    ext_dest_port = dest_port;
+    ext_dest_port = dest_port
     
     
     rec_port = random.randint(10000, 65000)
@@ -70,24 +105,54 @@ def handshake(dest_ip, dest_port, local_ip, domain):
         raise ValueError("Sockets didn't initialize successfully")
     
     
-    
     sock_send.sendto(packet, (dest_ip, ext_dest_port))
     
     
     rec = sock_rec.recv(1500)
     srcPort, destPort, seqNumber, ackNumber, raw_data, window = ip_handler.parse_TCP_packet(ip_handler.parse_IP_packet(rec))
     
-    getPacket = httpGet.craftRequest(domain, "/")
+    
     # Send the Ack
-    packet = ip_handler.make_tcp_header_2( b"", rec_port, ext_dest_port, initSqnc + 1, seqNumber + 1, 1, False, True, False, local_ip, dest_ip)
+    packet = ip_handler.make_tcp_header_2( b"", rec_port, ext_dest_port, ackNumber + 1, seqNumber + 1, 1, False, True, False, local_ip, dest_ip)
     packet = ip_handler.make_ip_header(packet, local_ip, dest_ip)
     sock_send.sendto(packet, (dest_ip, ext_dest_port))
     
+    """
     rec = sock_rec.recv(1500)
     srcPort, destPort, seqNumber, ackNumber, raw_data, window = ip_handler.parse_TCP_packet(ip_handler.parse_IP_packet(rec))
-    return sock_send, sock_rec
+    """
+    con = Connection(domain, subdomain, local_ip, dest_ip, ext_dest_port, send_port, rec_port)
+    con.setTCP(initSqnc + 1, seqNumber + 1)
+    con.setBindedSockets(sock_send, sock_rec)
+    return con
     
     
+    
+
+"""
+    This class holds the properties  of a connection. 
+"""
+class Connection():
+  def __init__(self, domain, subdomain, local_ip, dest_ip, dest_port, send_port, rec_port):
+    self.domain = domain
+    self.subdomain = subdomain
+    self.local_ip = local_ip
+    self.dest_ip = dest_ip
+    self.dest_port = dest_port
+    self.send_port = send_port
+    self.rec_port = rec_port
+    return
+
+def setTCP(self, seq_numb, ack_numb):
+    self.seq_numb = seq_numb
+    self.ack_numb = ack_numb
+    return
+    
+def setBindedSockets(self, send_sock, rec_sock):
+    self.send_sock = send_sock
+    self.rec_sock = rec_sock
+    return
+
     
 
 if __name__ == "__main__":
