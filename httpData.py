82c1dfaf84ab051eadd384bf284483ace376d641
Alan Garcia <alan_fgc@outlook.com>
2022-11-13 00:31:48 -0500

    Added chunks

--- httpData.py
+++ httpData.py
@@ -5,11 +5,15 @@ def stripHeader(header):
     return b"".join(header)
 
 def getChunks(data:list):
-    pData = []
+    chunks = []
+    
     for chunk in data:
-        pData.append(chunk[2:-1])
-    return pData
-
+        chunk = chunk.split(b"\r\n")
+        if chunk:
+            chunk = chunk[1:]
+        chunks.append(b"".join(chunk))
+    
+    return chunks
 
 if __name__ == "__main__":
     f = open("index.html", "r")
