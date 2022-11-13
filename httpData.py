f4e3e69a6758ac9f4ac6b553996cd5286f79823d
Alan Garcia <alan_fgc@outlook.com>
2022-11-13 11:53:25 -0500

    fixing chunks

--- httpData.py
+++ httpData.py
@@ -1,12 +1,19 @@
-
+"""
+Strip the header from http response
+"""
 def stripHeader(header):
     header = header.split(b"\r\n\r\n")
     header = header[1:]
     return b"".join(header)
 
+
+
+"""
+Strip metadata from chunked transfer encoding packets
+"""
 def getChunks(data:bytes):
     if data:
-        data = data[2:-1]
+        data = data[5:-1]
     return data
 
 if __name__ == "__main__":
