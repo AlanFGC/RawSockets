fb8b7b8cc17b6d5dc2e6053f12b068fc9a96f8c5
Alan Garcia <alan_fgc@outlook.com>
2022-11-12 23:39:04 -0500

    working on parsing chunks

--- httpData.py
+++ httpData.py
@@ -1,5 +1,11 @@
+
+def stripHeader(header):
+    header = header.split("\r\n")
+    
+    return header[1:]
+
 def getChunks(data:list):
     pData = []
     for chunk in data:
-        pData.append(chunk[-3])
+        pData.append(chunk[2:-1])
     return pData
